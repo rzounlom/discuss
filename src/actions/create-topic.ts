da482608aa906a181fb8de2d71e7b88c5a08f88c
Romaric Zounlome <rzounlom@gmail.com>
2024-02-19 12:39:12 -0500

    feat: add create topic form and validation flow

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,56 @@
 "use server";
 
-export async function createTopic() {
+import { auth } from "@/auth";
+import { z } from "zod";
+
+//schema for create topic
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z]+$/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  //valide incomeing form data
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this"],
+      },
+    };
+  }
+
+  return {
+    errors: {},
+  };
+
   // TODO: revalidate homepage
 }
