ca8b87e29e21e3e4162d024ae66757add9cc8e1a
Romaric Zounlome <rzounlom@gmail.com>
2024-02-19 11:45:47 -0500

    feat: complete auth flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,3 @@
-import * as actions from "@/actions";
-
-import { Button } from "@nextui-org/react";
-import Profile from "@/components/profile";
-import { auth } from "@/auth";
-
-export default async function Home() {
-  const session = await auth();
-
-  console.log({ session });
-
-  return (
-    <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign In</Button>
-      </form>
-      <form action={actions.signOut}>
-        <Button type="submit">Sign Out</Button>
-      </form>
-      {session?.user ? <div>Signed In</div> : <div>Signed Out</div>}
-
-      <Profile />
-    </div>
-  );
+export default function Home() {
+  return <div>Home Page</div>;
 }
