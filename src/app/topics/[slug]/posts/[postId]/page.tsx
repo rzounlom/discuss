b00396a3e6d6b52db48bb0345c923275200173ec
Romaric Zounlome <rzounlom@gmail.com>
2024-02-20 05:13:50 -0500

    feat: add suspese for loading post show page

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,10 +1,12 @@
+import { FC, Suspense } from "react";
+
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import CommentList from "@/components/comments/comment-list";
-import { FC } from "react";
 import Link from "next/link";
 import PostShow from "@/components/posts/post-show";
-import { fetchCommentByPostId } from "@/db/queries/comments";
+import PostShowLoading from "@/components/posts/post-show-loading";
 import paths from "@/paths";
+
 interface PostShowPageProps {
   params: {
     slug: string;
@@ -20,7 +22,10 @@ const PostShowPage: FC<PostShowPageProps> = ({ params }) => {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {"<"}Back to {slug}
       </Link>
-      <PostShow postId={postId} />
+      <Suspense fallback={<PostShowLoading />}>
+        <PostShow postId={postId} />
+      </Suspense>
+
       <CommentCreateForm postId={postId} startOpen />
       <CommentList postId={postId} />
     </div>
